af12a8d4238382b8ff99448005f216c6b3c4160d
tuannah <motchuchimnho@gmail.com>
2025-07-08 15:58:03 +0700

    add imgae 2

--- app/src/main/java/com/example/celeblookalike/ResultActivity.java
+++ app/src/main/java/com/example/celeblookalike/ResultActivity.java
@@ -3,7 +3,6 @@ package com.example.celeblookalike;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
